cc8486e5e9c12a1b1b94d6316d65275c5d41f174
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-05-27 08:24:59 +0000

    solved issue with logging parameters

--- Services/MeasurementProtocolLoggableTracker.php
+++ Services/MeasurementProtocolLoggableTracker.php
@@ -35,7 +35,7 @@ class MeasurementProtocolLoggableTracker extends MeasurementProtocolTracker
         $duration  = (microtime(true) - $startTime) * 1000;
 
         $this->logger->logRequest(
-            sprintf("[google analytics] sending hit %s to account %s with cid %s", $hitType, $args['tid'], $args['cid']),
+            sprintf("[google analytics] sending hit %s to account %s with cid %s", $hitType, $this->trackingID, $this->clientId),
             $duration,
             $results
         );
